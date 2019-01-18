9f54c1148a86fbe7844b2e05caa422e3bf7a3281
Kyle_Tecson <kyltecs@gmail.com>
2019-01-17 16:20:48 -0800

    update borrow item resolver

--- server/api/resolvers/index.js
+++ server/api/resolvers/index.js
@@ -94,17 +94,6 @@ module.exports = app => {
     },
 
     Item: {
-      /**
-       *  @TODO: Advanced resolvers
-       *
-       *  The Item GraphQL type has two fields that are not present in the
-       *  Items table in Postgres: itemowner, tags and borrower.
-       *
-       * According to our GraphQL schema, the itemowner and borrower should return
-       * a User (GraphQL type) and tags should return a list of Tags (GraphQL type)
-       *
-       */
-      // @TODO: Uncomment these lines after you define the Item type with these fields
       async itemowner(item, args, { pgResource }) {
         console.log(item);
         try {
@@ -121,15 +110,15 @@ module.exports = app => {
         } catch (e) {
           throw new ApolloError(e);
         }
+      },
+      async borrower(item, args, { pgResource }) {
+        try {
+          const itemBorrower = await pgResource.getUserById(item.borrowerid);
+          return itemBorrower;
+        } catch (e) {
+          throw ApolloError(e);
+        }
       }
-      // async borrower() {
-      //   /**
-      //    * @TODO: Replace this mock return statement with the correct user from Postgres
-      //    * or null in the case where the item has not been borrowed.
-      //    */
-      //   return null
-      //   // -------------------------------
-      // },
       // async imageurl({ imageurl, imageid, mimetype, data }) {
       //   if (imageurl) return imageurl
       //   if (imageid) {
