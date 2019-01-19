f5d07109952ab9e446c45100670d9df5e542bd25
Kyle_Tecson <kyltecs@gmail.com>
2019-01-19 15:06:16 -0800

    tags can now be added to items

--- server/api/pg-resource.js
+++ server/api/pg-resource.js
@@ -1,11 +1,6 @@
 const strs = require('stringstream');
 
 function tagsQueryString(tags, itemid, result) {
-  /**
-   * Challenge:
-   * This function is recursive, and a little complicated.
-   * Can you refactor it to be simpler / more readable?
-   */
   const length = tags.length;
   return length === 0
     ? `${result};`
@@ -40,7 +35,7 @@ module.exports = postgres => {
     },
     async getUserAndPasswordForVerification(email) {
       const findUserQuery = {
-        text: 'SELECT * FROM users WHERE email = $1',
+        text: 'SELECT * FROM users WHERE email = $1', //@TODO Authentication -- Server
         values: [email]
       };
       try {
@@ -52,28 +47,8 @@ module.exports = postgres => {
       }
     },
     async getUserById(id) {
-      /**
-       *  @TODO: Handling Server Errors
-       *
-       *  Inside of our resource methods we get to determine when and how errors are returned
-       *  to our resolvers using try / catch / throw semantics.
-       *
-       *  Ideally, the errors that we'll throw from our resource should be able to be used by the client
-       *  to display user feedback. This means we'll be catching errors and throwing new ones.
-       *
-       *  Errors thrown from our resource will be captured and returned from our resolvers.
-       *
-       *  This will be the basic logic for this resource method:
-       *  1) Query for the user using the given id. If no user is found throw an error.
-       *  2) If there is an error with the query (500) throw an error.
-       *  3) If the user is found and there are no errors, return only the id, email, fullname, bio fields.
-       *     -- this is important, don't return the password!
-       *
-       *  You'll need to complete the query first before attempting this exercise.
-       */
-
       const findUserQuery = {
-        text: 'SELECT id, email, name AS fullname FROM users WHERE id = $1',
+        text: 'SELECT id, email, name AS fullname FROM users WHERE id = $1', //@TODO Authentication -- Server
         values: [id]
       };
       try {
@@ -171,24 +146,19 @@ module.exports = postgres => {
                 // Image has been converted, begin saving things
                 const { title, description, tags } = item;
 
-                // Generate new Item query
-                // @TODO
-                // -------------------------------
                 const newItemQuery = {
                   text:
-                    'INSERT INTO users(title, description, tags ) VALUES($1, $2, $3)',
-                  values: [title, description, tags]
+                    'INSERT INTO users(title, description, ownerid ) VALUES($1, $2, $3) RETURNING *',
+                  values: [title, description, user.id]
                 };
 
-                // Insert new Item
-                // @TODO
-                // -------------------------------
+                const createdItem = await postgres.query(newItemQuery);
 
                 const imageUploadQuery = {
                   text:
                     'INSERT INTO uploads (itemid, filename, mimetype, encoding, data) VALUES ($1, $2, $3, $4, $5) RETURNING *',
                   values: [
-                    // itemid,
+                    itemid,
                     image.filename,
                     image.mimetype,
                     'base64',
@@ -208,13 +178,16 @@ module.exports = postgres => {
                 // @TODO
                 // -------------------------------
 
-                // Generate tag relationships query (use the'tagsQueryString' helper function provided)
-                // @TODO
-                // -------------------------------
+                const tagRelationshipQuery = {
+                  text: `INSERT INTO itemtags(itemid, tagsid) VALUES(${tagsQueryString(
+                    [...tags],
+                    itemid,
+                    ''
+                  )}) `,
+                  values: tags.map(tag => tag.id)
+                };
 
-                // Insert tags
-                // @TODO
-                // -------------------------------
+                const addTag = await postgres.query(tagRelationshipQuery);
 
                 // Commit the entire transaction!
                 client.query('COMMIT', err => {
